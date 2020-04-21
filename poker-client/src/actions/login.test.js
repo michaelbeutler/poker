d97a32f30f04ca82a79403951b971a2c13fc1e74
MichiBeutler <michael.beutler@gibmit.ch>
2020-04-21 16:13:54 +0200

    added testting for actions :white_check_mark:

---
+++ poker-client/src/actions/login.test.js
@@ -0,0 +1,25 @@
+import * as actions from './login'
+
+describe('actions', () => {
+    it('should create an action to set login success', () => {
+        const data = {
+            username: "Test",
+            id: "1234"
+        }
+        const expectedAction = {
+            type: actions.LOGIN_SUCCESS,
+            ...data
+        }
+        expect(actions.loginSuccess(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set login failure', () => {
+        const data = {
+            text: "Test"
+        }
+        const expectedAction = {
+            type: actions.LOGIN_ERROR,
+            ...data
+        }
+        expect(actions.loginError(data)).toEqual(expectedAction)
+    })
+});
