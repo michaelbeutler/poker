51698926f14a4cca5f8d14c8a703c065ed9ac12d
MichiBeutler <michael.beutler@gibmit.ch>
2020-04-22 16:48:16 +0200

    added redux actions test :white_check_mark:

--- poker-client/src/actions/game.test.js
+++ poker-client/src/actions/game.test.js
@@ -2,13 +2,65 @@ import * as actions from './game'
 
 describe('actions for game', () => {
     it('should create an action to set create game success', () => {
-        const data = {
-            id: "1234"
-        }
+        const data = { id: "1234" }
         const expectedAction = {
             type: actions.CREATE_GAME_SUCCESS,
             ...data
         }
         expect(actions.createGameSuccess(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set create game error', () => {
+        const data = { text: "1234" }
+        const expectedAction = {
+            type: actions.CREATE_GAME_ERROR,
+            ...data
+        }
+        expect(actions.createGameError(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set join game success', () => {
+        const data = { id: "1234" }
+        const expectedAction = {
+            type: actions.JOIN_GAME_SUCCESS,
+            ...data
+        }
+        expect(actions.joinGameSuccess(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set join game error', () => {
+        const data = { text: "1234" }
+        const expectedAction = {
+            type: actions.JOIN_GAME_ERROR,
+            ...data
+        }
+        expect(actions.joinGameError(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set join game', () => {
+        const data = { id: "1234", username: "test" }
+        const expectedAction = {
+            type: actions.JOIN_GAME,
+            ...data
+        }
+        expect(actions.joinGame(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set leave game success', () => {
+        const expectedAction = {
+            type: actions.LEAVE_GAME_SUCCESS,
+        }
+        expect(actions.leaveGameSuccess()).toEqual(expectedAction)
+    })
+    it('should create an action to set leave game error', () => {
+        const data = { text: "1234" }
+        const expectedAction = {
+            type: actions.LEAVE_GAME_ERROR,
+            ...data
+        }
+        expect(actions.leaveGameError(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set leave game', () => {
+        const data = { id: "1234", username: "test" }
+        const expectedAction = {
+            type: actions.LEAVE_GAME,
+            ...data
+        }
+        expect(actions.leaveGame(data)).toEqual(expectedAction)
     })
 });
--- poker-client/src/actions/login.test.js
+++ poker-client/src/actions/login.test.js
@@ -2,24 +2,27 @@ import * as actions from './login'
 
 describe('actions for login', () => {
     it('should create an action to set login success', () => {
-        const data = {
-            username: "Test",
-            id: "1234"
-        }
+        const data = { username: "test", id: "1234" }
         const expectedAction = {
             type: actions.LOGIN_SUCCESS,
             ...data
         }
         expect(actions.loginSuccess(data)).toEqual(expectedAction)
     })
-    it('should create an action to set login failure', () => {
-        const data = {
-            text: "Test"
-        }
+    it('should create an action to set login error', () => {
+        const data = { text: "test" }
         const expectedAction = {
             type: actions.LOGIN_ERROR,
             ...data
         }
         expect(actions.loginError(data)).toEqual(expectedAction)
+    })
+    it('should create an action to set login request', () => {
+        const data = { text: "test" }
+        const expectedAction = {
+            type: actions.LOGIN_REQUIRED,
+            ...data
+        }
+        expect(actions.loginRequired(data)).toEqual(expectedAction)
     })
 });
